575f6c612c56d063d1ed501bf5aae18ca3473c6e
itsrag@gmail.com <itsrag@gmail.com>
2012-12-11 20:07:26 -0600

    Pushing noun handling changes

--- src/util/Constants.java
+++ src/util/Constants.java
@@ -1,11 +1,11 @@
 package util;
 
 public interface Constants {
-	final static String WORD_POLARITY_SRC_FILENAME = "3in1_adj.properties";
-	public static final String INCONSISTENT_POLAIRY_OUTPUT_FILE = "output.txt";
-	String WORDNET_SENSES_FREQ = "wordnet_sense_freq.properties";
-	String EXISTING_INCONST_PROP = "InconsistentWordPolarity.properties";
-	String OUTPUT_PROP_FILE = "UpdatedWordPolarity.properties";
+	final static String WORD_POLARITY_SRC_FILENAME = "3in1_noun.properties";
+	public static final String INCONSISTENT_POLAIRY_OUTPUT_FILE = "output_noun.txt";
+	String WORDNET_SENSES_FREQ = "wordnet_sense_freq_noun.properties";
+	String EXISTING_INCONST_PROP = "InconsistentWordPolarity_noun.properties";
+	String OUTPUT_PROP_FILE = "UpdatedWordPolarity_noun.properties";
 	
 	
 	
